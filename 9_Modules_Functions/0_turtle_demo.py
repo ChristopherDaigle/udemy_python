8bc6335251c2da4f621d506f5ac580c7c8311fcc
Chris Daigle <chris.daigle@uconn.edu>
2020-03-30 13:01:20 -0400

    feat: begin python standard library section

---
+++ 9_Modules_Functions/0_turtle_demo.py
@@ -0,0 +1,8 @@
+import turtle as t
+# import time
+
+t.forward(150)
+t.right(250)
+t.forward(150)
+# time.sleep(2)
+t.done()
---
+++ 9_Modules_Functions/1_turtle_demo.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+forward(150)
+right(250)
+circle(75)
+forward(150)
+
+done()
